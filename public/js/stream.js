27fbc85c568bcbbc2b9617a31d6bab1fda2cc0ee
Bassel Ahmed <bassel.ahmed@outlook.com>
2018-04-16 12:01:18 +0200

    Testing...

--- public/js/stream.js
+++ public/js/stream.js
@@ -1,17 +1,19 @@
-var io
+var io;
 
-$(document).ready(function () {
-  const socket = io('/client')
+$(document).ready(function() {
+  const socket = io("/client");
 
-  socket.on('connect', function () {
-    console.log('connected to socket')
-  })
-  socket.on('hello', function () {
-    console.log('hello')
-    window.alert('Android conencted to server')
-  })
-  socket.on('frame', function (data) {
-    console.log(data)
-    window.alert('frame sent')
-  })
-})
+  socket.on("connect", function() {
+    console.log("connected to socket");
+  });
+  socket.on("hello", function() {
+    console.log("hello");
+    window.alert("Android conencted to server");
+  });
+  socket.on("frame", function(data) {
+    console.log(data);
+    // window.alert('frame sent')
+    console.log(new Blob([new Uint8Array(data.frame)]));
+    // $(".stream-el");
+  });
+});
