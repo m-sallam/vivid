cd003cf61c6219b6276ee78d686fdab818a82b5e
Bassel Ahmed <bassel.ahmed@outlook.com>
2018-04-16 12:09:47 +0200

    Testing...

--- public/js/stream.js
+++ public/js/stream.js
@@ -16,6 +16,7 @@ $(document).ready(function() {
     var blob = new Blob([new Uint8Array(data.frame)]);
     console.log(blob);
     $("#stream-el").attr("src", blob);
+
     // var video = document.getElementById("stream-el");
     // video.src = window.URL.createObjectURL(vid);
   });
