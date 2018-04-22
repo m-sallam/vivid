d7b073bd4224ee860dba1e5cf0db7b8472eef5e9
m-sallam <m-sallam@users.noreply.github.com>
2018-04-22 15:46:34 +0200

    added temp routes

--- routes/temp.js
+++ routes/temp.js
@@ -4,6 +4,7 @@ const fs = require('fs')
 const IO = require('koa-socket-2')
 const videoIO = new IO({ namespace: 'video' })
 const ss = require('socket.io-stream')
+const User = require('../models/user')
 
 router.get('/temp/stream', async ctx => {
   await ctx.render('stream')
@@ -22,6 +23,12 @@ router.get('/temp/video', async ctx => {
   ctx.type = 'mp4'
   ctx.body = fs.createReadStream(path)
 })
+
+router.get('/users', async ctx => {
+  let users = await User.find({})
+  ctx.body = users
+})
+
 ss(videoIO).on('frame', (stream, data) => {
   console.log('s')
   stream.pip(fs.createWriteStream('./q.mp4'))
