ba7bd1df12c26168b886d5c0b5c591919747ad35
Bassel Ahmed <bassel.ahmed@outlook.com>
2018-06-12 23:50:52 +0200

    Fixes

--- assets/js/login.js
+++ assets/js/login.js
@@ -1,33 +1,47 @@
-$(document).ready(function () {
-  $('.text-input').on('focusout change', function () {
-    if ($(this).val()) {
-      $(this).addClass('notEmpty')
-    } else {
-      $(this).removeClass('notEmpty')
+$(document)
+  .ready(function () {
+
+    function checkInputs() {
+      if ($(this).val()) {
+        $(this).addClass('notEmpty')
+      } else {
+        $(this).removeClass('notEmpty')
+      }
+    }
+    checkInputs()
+    $('.text-input').on('focusout change', function () {
+      checkInputs()
+    })
+
+    if ($('#languages-select').length > 0) {
+      $('#languages-select').selectize({
+        // placeholder: "Languages"
+        onFocus: function () {
+          // console.log(this)
+          var wrapper = this.$wrapper
+          wrapper.addClass('focused')
+          wrapper
+            .siblings('.text-input-label')
+            .addClass('focused')
+        },
+        onDropdownClose: function () {
+          var wrapper = this.$wrapper
+          wrapper.removeClass('focused')
+          wrapper
+            .siblings('.text-input-label')
+            .removeClass('focused')
+          if (this.items.length !== 0) {
+            wrapper.addClass('focused')
+            wrapper
+              .siblings('.text-input-label')
+              .addClass('focused')
+          } else {
+            wrapper.removeClass('focused')
+            wrapper
+              .siblings('.text-input-label')
+              .removeClass('focused')
+          }
+        }
+      })
     }
   })
-
-  if ($('#languages-select').length > 0) {
-    $('#languages-select').selectize({
-      // placeholder: "Languages"
-      onFocus: function () {
-        // console.log(this)
-        var wrapper = this.$wrapper
-        wrapper.addClass('focused')
-        wrapper.siblings('.text-input-label').addClass('focused')
-      },
-      onDropdownClose: function () {
-        var wrapper = this.$wrapper
-        wrapper.removeClass('focused')
-        wrapper.siblings('.text-input-label').removeClass('focused')
-        if (this.items.length !== 0) {
-          wrapper.addClass('focused')
-          wrapper.siblings('.text-input-label').addClass('focused')
-        } else {
-          wrapper.removeClass('focused')
-          wrapper.siblings('.text-input-label').removeClass('focused')
-        }
-      }
-    })
-  }
-})
