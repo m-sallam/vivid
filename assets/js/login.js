28c20c928c0d99bf30313eb389c47b1aeecdb8c5
Muhammad <muhammad.sallam@outlook.com>
2018-06-12 23:47:13 +0200

    fixes

--- assets/js/login.js
+++ assets/js/login.js
@@ -7,25 +7,27 @@ $(document).ready(function () {
     }
   })
 
-  $('#languages-select').selectize({
-    // placeholder: "Languages"
-    onFocus: function () {
-      // console.log(this)
-      var wrapper = this.$wrapper
-      wrapper.addClass('focused')
-      wrapper.siblings('.text-input-label').addClass('focused')
-    },
-    onDropdownClose: function () {
-      var wrapper = this.$wrapper
-      wrapper.removeClass('focused')
-      wrapper.siblings('.text-input-label').removeClass('focused')
-      if (this.items.length !== 0) {
+  if ($('#languages-select').length > 0) {
+    $('#languages-select').selectize({
+      // placeholder: "Languages"
+      onFocus: function () {
+        // console.log(this)
+        var wrapper = this.$wrapper
         wrapper.addClass('focused')
         wrapper.siblings('.text-input-label').addClass('focused')
-      } else {
+      },
+      onDropdownClose: function () {
+        var wrapper = this.$wrapper
         wrapper.removeClass('focused')
         wrapper.siblings('.text-input-label').removeClass('focused')
+        if (this.items.length !== 0) {
+          wrapper.addClass('focused')
+          wrapper.siblings('.text-input-label').addClass('focused')
+        } else {
+          wrapper.removeClass('focused')
+          wrapper.siblings('.text-input-label').removeClass('focused')
+        }
       }
-    }
-  })
+    })
+  }
 })
